5ac4a616fac7eb8ef4dcf078a7b814cb2c00b56a
lavaspoon <lavaspoon@naver.com>
2021-11-06 22:59:02 +0900

    프로젝트 생성

---
+++ calculate/src/main/java/lavaspoon/calculate/ServletInitializer.java
@@ -0,0 +1,13 @@
+package lavaspoon.calculate;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CalculateApplication.class);
+	}
+
+}
