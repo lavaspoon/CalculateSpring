1864e86b0c03b9971394c6a4cf88e81599b7cb31
lavaspoon <lavaspoon@naver.com>
2021-11-10 23:13:37 +0900

    유연한 컨트롤러 적용

---
+++ calculate/src/main/java/lavaspoon/calculate/web/springmvc/old/OldController.java
@@ -0,0 +1,19 @@
+package lavaspoon.calculate.web.springmvc.old;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//스프링빈의 이름(OldController)이 "/springmvc/old-controller" 이게됨
+@Component("/springmvc/old-controller")
+public class OldController implements Controller {
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("OldController.handleRequest");
+        return null;
+    }
+}
